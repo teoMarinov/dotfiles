783eb11683504981c33ce46faaac08acd648a5fb
Teodor Marinov <teodor.b.marinov@gmail.com>
2026-06-11 12:07:40 +0300

    Sync before change to lua

---
+++ config/hypr/scripts/ToggleTouchpad.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+DEV="gxtp5100:00-27c6:01e0-touchpad"
+STATE_FILE="$HOME/.cache/touchpad_disabled"
+iDIR="$HOME/.config/swaync/images"
+
+if [[ -f "$STATE_FILE" ]]; then
+  hyprctl keyword "device[$DEV]:enabled" true >/dev/null
+  rm -f "$STATE_FILE"
+  notify-send -i "$iDIR/info.png" "Touchpad" "Enabled"
+else
+  hyprctl keyword "device[$DEV]:enabled" false >/dev/null
+  touch "$STATE_FILE"
+  notify-send -i "$iDIR/info.png" "Touchpad" "Disabled"
+fi
